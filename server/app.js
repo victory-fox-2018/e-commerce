49d0e1bdce07c4e2ad7622ea6fffbe94a7f7b301
ivan <ivansu.webmail@gmail.com>
2018-09-11 09:32:19 +0700

    set up

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/shopping-cart', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to mongooese...'));
+
+app.use(cors());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.listen(process.env.PORT || '3000', () => console.log('Connected to server...'));
