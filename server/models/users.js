b1e7d643f5b4be39d3467472d49a35d3e97dc65a
vinesto <oqvinesto@gmail.com>
2018-09-11 07:36:49 +0700

    login and register done

---
+++ server/models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+var userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, minlength: 6 },
+}, {
+        timestamps: true
+    })
+
+userSchema.pre('save', function (next) {
+    let user = this
+    if (!user.isModified('password')) {
+        return next()
+    }
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err)
+        }
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) {
+                return next(err)
+            }
+            user.password = hash
+            next()
+        })
+    })
+})
+
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
