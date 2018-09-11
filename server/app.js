fced69d233b386b1ab7f9ac6a7e3ff85d944dfe4
Renaldy Pratama <renaldy.pratama19@gmail.com>
2018-09-11 19:49:02 +0700

    new index.html using Vue.js, showing item & category done, progress cart system

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
