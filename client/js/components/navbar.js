17ae52bf9cb87ea6bf74ee15d3900d3d0401784b
ivan <ivansu.webmail@gmail.com>
2018-09-15 17:58:03 +0700

    show modal signin and signup

--- client/js/components/navbar.js
+++ client/js/components/navbar.js
@@ -1,7 +1,58 @@
 Vue.component('navbar', {
   data() {
     return {
+      signin: {
+        modal: false,
+        email: '',
+        password: ''
+      },
+      signup: {
+        modal: false,
+        name: '',
+        email: '',
+        password: ''
+      },
+      showLogout: false
+    }
+  },
+  methods: {
+    doSignIn() {
+      let self = this;
 
+      axios({
+        method: 'POST',
+        url: `${baseurl}/customers/signin`,
+        data: {
+          email: self.signin.email,
+          password: self.signin.password
+        }
+      })
+      .then(response => {
+
+      })
+      .catch(err => {
+
+      });
+    },
+
+    doSignUp() {
+      console.log('sign up...');
+    },
+ 
+    openSignIn() {
+      this.signin.modal = true;
+    },
+
+    openSignUp() {
+      this.signup.modal = true;
+    },
+
+    closeSignIn() {
+      this.signin.modal = false;
+    },
+
+    closeSignUp() {
+      this.signup.modal = false;
     }
   },
   template: `
@@ -43,13 +94,18 @@ Vue.component('navbar', {
                 </a>
               </div>
               <div class="control">
-                <a href="javascript:void(0)" class="button is-info is-inverted">
+                <a href="javascript:void(0)" class="button is-info is-inverted" @click="openSignIn">
                   <span class="icon fa fa-sign-in-alt"></span> <span>Sign In</span>
                 </a>
               </div>
               <div class="control">
-                <a class="button is-info" href="javascript:void(0)">
+                <a class="button is-info" href="javascript:void(0)" @click="openSignUp">
                   <span class="icon fa fa-plus"></span> <span>Sign Up</span>
+                </a>
+              </div>
+              <div class="control" v-if="logoutBtn">
+                <a class="button is-danger" href="javascript:void(0)">
+                  <span class="icon fa fa-sign-out-alt"></span> <span>Sign Out</span>
                 </a>
               </div>
             </div>
@@ -57,17 +113,17 @@ Vue.component('navbar', {
         </div>
       </div>
       
-      <div class="modal">
+      <div class="modal is-active" v-if="signin.modal">
         <div class="modal-background"></div>
         <div class="modal-card">
           <header class="modal-card-head">
             <p class="modal-card-title">Sign In</p>
-            <button class="delete" aria-label="close"></button>
+            <button class="delete" aria-label="close" @click="closeSignIn"></button>
           </header>
           <section class="modal-card-body">
             <div class="field">
               <p class="control has-icons-left has-icons-right">
-                <input class="input is-medium" type="email" placeholder="Email">
+                <input class="input is-medium" type="email" placeholder="Email" v-model="signin.email">
                 <span class="icon is-left">
                   <i class="fas fa-envelope"></i>
                 </span>
@@ -78,7 +134,7 @@ Vue.component('navbar', {
             </div>
             <div class="field">
               <p class="control has-icons-left">
-                <input class="input is-medium" type="password" placeholder="Password">
+                <input class="input is-medium" type="password" placeholder="Password" v-model="signin.password">
                 <span class="icon is-left">
                   <i class="fas fa-lock"></i>
                 </span>
@@ -86,23 +142,23 @@ Vue.component('navbar', {
             </div>
           </section>
           <footer class="modal-card-foot">
-            <button class="button is-medium is-success">Sign In</button>
-            <button class="button is-medium">Cancel</button>
+            <button class="button is-medium is-success" @click="doSignIn">Sign In</button>
+            <button class="button is-medium" @click="closeSignIn">Cancel</button>
           </footer>
         </div>
       </div>
 
-      <div class="modal">
+      <div class="modal is-active" v-if="signup.modal">
         <div class="modal-background"></div>
         <div class="modal-card">
           <header class="modal-card-head">
             <p class="modal-card-title">Sign Up</p>
-            <button class="delete" aria-label="close"></button>
+            <button class="delete" aria-label="close" @click="closeSignUp"></button>
           </header>
           <section class="modal-card-body">
             <div class="field">
               <p class="control has-icons-left has-icons-right">
-                <input class="input is-medium" type="text" placeholder="Name">
+                <input class="input is-medium" type="text" placeholder="Name" v-model="signup.name">
                 <span class="icon is-left">
                   <i class="fas fa-user"></i>
                 </span>
@@ -113,7 +169,7 @@ Vue.component('navbar', {
             </div>
             <div class="field">
               <p class="control has-icons-left has-icons-right">
-                <input class="input is-medium" type="email" placeholder="Email">
+                <input class="input is-medium" type="email" placeholder="Email" v-model="signup.email">
                 <span class="icon is-left">
                   <i class="fas fa-envelope"></i>
                 </span>
@@ -124,7 +180,7 @@ Vue.component('navbar', {
             </div>
             <div class="field">
               <p class="control has-icons-left">
-                <input class="input is-medium" type="password" placeholder="Password">
+                <input class="input is-medium" type="password" placeholder="Password" v-model="signup.password">
                 <span class="icon is-left">
                   <i class="fas fa-lock"></i>
                 </span>
@@ -132,8 +188,8 @@ Vue.component('navbar', {
             </div>
           </section>
           <footer class="modal-card-foot">
-            <button class="button is-medium is-success">Sign Up</button>
-            <button class="button is-medium">Cancel</button>
+            <button class="button is-medium is-success" @click="doSignUp">Sign Up</button>
+            <button class="button is-medium" @click="closeSignUp">Cancel</button>
           </footer>
         </div>
       </div>
