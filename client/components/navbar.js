5fbd07d2fee5316ded164379269c5c93caa776ad
vinesto <oqvinesto@gmail.com>
2018-09-15 21:58:25 +0700

    switch navbar to vue component

---
+++ client/components/navbar.js
@@ -0,0 +1,263 @@
+Vue.component('nav-bar', {
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#"><i class="fas fa-fish"></i></a>
+                <a class="navbar-brand" href="#">Amisquarium</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <input class="form-control" type="text" placeholder="Search item here">
+                        </li>
+
+                        <li class="nav-item" v-if="token">
+                            <div class="dropdown">
+                                <button class="btn btn-transaparent dropdown-toggle bg-transparent" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    My Transaction
+                                    <i class="fas fa-money-bill"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <div class="card-fluid">
+                                        <div class="card-header bg-transparent">My History Transaction</div>
+                                        <div class="card-body">
+                                            <table class="table table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">No</th>
+                                                        <th scope="col">Name</th>
+                                                        <th scope="col">Price</th>
+                                                        <th scope="col">Quantity</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <th scope="row">1</th>
+                                                        <td>Mark</td>
+                                                        <td>Otto</td>
+                                                        <td>@mdo</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th scope="row">2</th>
+                                                        <td>Jacob</td>
+                                                        <td>Thornton</td>
+                                                        <td>@fat</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="nav-item" v-if="token">
+                            <div class="dropdown">
+                                <button class="btn btn-transaparent dropdown-toggle bg-transparent" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Cart
+                                    <i class="fas fa-shopping-cart"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <div class="card-fluid">
+                                        <div class="card-header bg-transparent">My Cart</div>
+                                        <div class="card-body">
+                                            <table class="table table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">No</th>
+                                                        <th scope="col">Name</th>
+                                                        <th scope="col">Price</th>
+                                                        <th scope="col">Quantity</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <th scope="row">1</th>
+                                                        <td>Mark</td>
+                                                        <td>Otto</td>
+                                                        <td>@mdo</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th scope="row">2</th>
+                                                        <td>Jacob</td>
+                                                        <td>Thornton</td>
+                                                        <td>@fat</td>
+                                                    </tr>
+
+                                                </tbody>
+                                            </table>
+                                            <div>
+                                                Total Price : asdaddsa
+                                            </div>
+                                            <button type="button" class="btn btn-info">Checkout</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li class="nav-item" v-if="!token">
+                            <div class="dropdown">
+                                <button class="btn btn-transaparent dropdown-toggle bg-transparent" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Login
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <div class="card-fluid">
+                                        <div class="card-header bg-transparent">Login Form</div>
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                                Email:
+                                                <input class="form-control-sm" type="text" placeholder="Your email"
+                                                    v-model="loginEmail">
+                                                Password:
+                                                <input class="form-control-sm" type="password" placeholder="Your password"
+                                                    v-model="loginPassword">
+                                            </div>
+                                            <button type="button" class="btn btn-info" v-on:click="login">Submit</button>
+                                        </div>
+                                        <div class="card-footer bg-transparent" v-if="errorMessage.length > 0" v-for="(message, index) in errorMessage" :key="index">footer {{message}}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="nav-item" v-if="token">
+                            <button type="button" class="btn bg-transparent" v-on:click="logout">Logout</button>
+                        </li>
+
+                        <li class="nav-item" v-if="!token">
+                            <div class="dropdown">
+                                <button class="btn btn-transaparent dropdown-toggle bg-transparent" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Register
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <div class="card-fluid">
+                                        <div class="card-header bg-transparent">Register Form</div>
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                            
+                                                <label for="registerName">Name:</label>
+                                                <input class="form-control-sm" type="text" id="registerName" placeholder="Your email"
+                                                    v-model="registerName">
+                                                <label for="registerEmail">Email:</label>
+                                                <input class="form-control-sm" type="email" id="registerEmail" placeholder="Your email"
+                                                    v-model="registerEmail">
+                                                    <label for="registerEmail">Password:</label>
+                                                <input class="form-control-sm" type="password" id="registerPassword" placeholder="Your email"
+                                                    v-model="registerPassword">
+                                            </div>
+                                            <button type="button" class="btn btn-info" v-on:click="register">Submit</button>
+                                        </div>
+                                        <div class="card-footer bg-transparent">footer</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `,
+    data: function(){
+
+        return{
+            token:false,
+            loginName: '',
+            loginEmail: '',
+            loginPassword: '',
+            registerName:'',
+            registerEmail: '',
+            registerPassword: '',
+            errorMessage: [],
+            message: '',
+            item: [],
+            cart: [],
+            total: 0,
+            isLogin:''
+        }
+    },
+    methods: {
+        login() {
+            let self = this
+            this.errorMessage = []
+                axios({
+                    method: "POST",
+                    url: 'http://localhost:3000/users/login',
+                    data: {
+                        email: this.loginEmail,
+                        password: this.loginPassword
+                    }
+                })
+                .then(function ({data}) {
+                    self.errorMessage = []
+                    localStorage.setItem("token", data.token)
+                    self.isLogin = localStorage.getItem("token")
+                    self.loginEmail = ''
+                    self.loginPassword = ''
+                })
+                .catch(function (err) {
+                    // console.log('masuuk catch');
+                    // console.log(err.response.data.message);
+                    self.errorMessage.push(err.response.data.message)
+                })
+            // }
+        },
+        register() {
+            let self = this
+            axios({
+                method: "POST",
+                url: "http://localhost:3000/users/register",
+                data:   {
+                    name: this.registerName,
+                    email: this.registerEmail,
+                    password: this.registerPassword
+                }
+            })
+                .then(function (result) {
+                    // alert("register success")
+                    console.log(result)
+                    self.errorMessage=[]
+                    self.message = "Register success"
+                    self.registerName = ''
+                    self.registerEmail = ''
+                    self.registerPassword = ''
+                })
+                .catch(function (error) {
+                    self.message = error.response.data.message
+                    console.log(error.response.data.message);
+                    
+                    // alert("register failed")
+                })
+        },
+        logout(){
+            // this.isLogout = localStorage.getItem("token")
+            localStorage.clear()
+            this.token = false,
+            this.isLogin = ''
+        }
+    },
+    created() {
+        let checkToken = localStorage.getItem("token")
+        if(checkToken){
+            this.token = true
+        }
+    },
+    watch:{
+        isLogin:function(newLogin, oldLogin){
+            if(newLogin){
+                this.token = true
+                this.$emit('check-token',this.token)
+            }
+
+        }
+    }
+})
--- client/index.js
+++ client/index.js
@@ -1,39 +1,74 @@
-var token = localStorage.getItem("token")
+// var token = localStorage.getItem("token")
 
 var app = new Vue({
     el: '#app',
     data: {
-        item: [],
+        items: [],
         cart: [],
-        total: 0
+        total: 0,
+        token: false,
+        Login: {
+            name:'',
+            email: '',
+            password: ''
+        },
+        Register: {
+            name: '',
+            email: '',
+            password: ''
+        },
+        errorMessage: [],
+        message: '',
+        isLogout:'',
+        isLogin:''
     },
     methods: {
-        login() {
-            axios({
-                method: "POST",
-                url: 'http://localhost:3000/users/login'
-            })
-                .then(function ({ data }) {
-                    alert("login success")
-                    console.log(data);
-                })
-                .catch(function (error) {
-                    alert("login failed")
-                })
-        },
-        register() {
-            axios({
-                method: "POST",
-                url: "http://localhost:3000/users/register"
-            })
-                .then(function ({ data }) {
-                    alert("register success")
-                    console.log(data);
-                })
-                .catch(function (error) {
-                    alert("register failed")
-                })
-        },
+        // login() {
+        //     console.log(this.token);
+            
+        //     let self = this
+        //     this.errorMessage = []
+        //         axios({
+        //             method: "POST",
+        //             url: 'http://localhost:3000/users/login',
+        //             data: {
+        //                 email: this.Login.email,
+        //                 password: this.Login.password
+        //             }
+        //         })
+        //         .then(function ({data}) {
+        //             self.errorMessage = []
+        //             localStorage.setItem("token", data.token)
+        //             self.isLogin = localStorage.getItem("token")
+        //         })
+        //         .catch(function (err) {
+        //             console.log('masuuk catch');
+        //             console.log(err.response.data.message);
+        //             self.errorMessage.push(err.response.data.message)
+        //         })
+        //     // }
+        // },
+        // register() {
+        //     axios({
+        //         method: "POST",
+        //         url: "http://localhost:3000/users/register",
+        //         data:   {
+        //             name: this.Register.name,
+        //             email: this.Register.email,
+        //             password: this.Register.password
+        //         }
+        //     })
+        //         .then(function (result) {
+        //             // alert("register success")
+        //             console.log(result)
+        //             self.errorMessage=[]
+        //         })
+        //         .catch(function (error) {
+        //             console.log(error.response.data.message);
+                    
+        //             // alert("register failed")
+        //         })
+        // },
         getAllItem() {
             let self = this
             axios({
@@ -41,18 +76,41 @@ var app = new Vue({
                 url: "http://localhost:3000/items"
             })
                 .then(function ({ data }) {
+                    console.log(data.data);
+                    
                     self.items = data.data
                 })
-                .catch(function(err){
+                .catch(function (err) {
                     console.log(err.message);
                 })
-        }
+        },
+        // logout(){
+        //     this.isLogout = localStorage.getItem("token")
+        //     localStorage.clear()
+        // }
     },
-    mounted() {
+    created() {
+        let checkToken = localStorage.getItem("token")
+        if(checkToken){
+            this.token = true
+        }
         this.getAllItem()
     },
-    watch:{
-        cart:this.cart
+    watch: {
+        isLogout:function(newLogout,oldLogout){
+            if(newLogout){
+                this.token = false
+            }
+        },
+        isLogin:function(newLogin,oldLogin){
+            if(newLogin){
+                this.token = true  
+            }
+        },
+    },
+    computed:{
+        priceTag:function(){
+            
+        }
     }
-
 })
