6ce5ecd3196cc3db56b230020bd7f3c0845f0801
ivan <ivansu.webmail@gmail.com>
2018-09-16 14:44:02 +0700

    delete old component files

--- client/js/footer.js
+++
@@ -1,9 +0,0 @@
-Vue.component('main-footer', {
-  template: `
-    <footer>
-      <div class="container">
-        <p class="footer-info">&copy; <span class="font-brand">TutupLapak</span> - 2018</p>
-      </div>
-    </footer>
-  `
-});
