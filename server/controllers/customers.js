2d4fa96408f6f13463f9b65d3617ba3e4dcc2795
hacktiv8 <hacktiv8@android-18a3e69bf9b03a25.localdomain>
2018-09-17 17:12:04 +0700

    safe

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -4,24 +4,19 @@ const { encrypt } = require('../helpers/encrypt');
 
 module.exports = {
   signin: (req, res) => {
-    
     Customer
       .findOne({
         email: req.body.email,
         password: encrypt(req.body.password)
       })
       .then(customer => {
-        
         if(customer) {
           const id = customer._id;
-          
-          jwt.sign({
-            id,
+          jwt.sign({id,
             name: customer.name,
             email: customer.email
           }, process.env.SECRET, (err, token) => {
-            
-            res.status(200).json({
+              res.status(200).json({
               message: `Berhail masuk`,
               token, name: customer.name
             })
@@ -34,12 +29,9 @@ module.exports = {
         res.status(500).json({ message: err });
       })
 
-    
   },
 
   signup: (req, res) => {
-    console.log(req.body);
-    
     const newCustomer = new Customer({
       name: req.body.name,
       email: req.body.email,
@@ -48,10 +40,10 @@ module.exports = {
     });
 
     newCustomer
-      .save()
-      .then(Customer => {
-        res.status(200).json({
-          message: `Selamat ${Customer.name}, sekarang kamu sudah terdaftar sebagai Customer Borong-borong `
+    .save()
+    .then(Customer => {
+      res.status(200).json({
+        message: `Selamat ${Customer.name}, sekarang kamu sudah terdaftar sebagai Customer Borong-borong `
         })
       })
       .catch(err => {
