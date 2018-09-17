080b18f4f285244712a7959afd2df360b4dafc94
imamaguss <imam.agussopian@gmail.com>
2018-09-17 09:09:19 +0700

    move cart

--- server/controllers/carts.js
+++ server/controllers/carts.js
@@ -5,54 +5,53 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
   create: (req, res) => {
+    console.log('req.body ===> ', req.body);
     const token = req.body.token;
     jwt.verify(token, process.env.SECRET, (err, decode) => {
-      console.log(decode);
+      console.log('decode =====> ', decode);
       let totalPrice = 0;
-      if (typeof req.body.data == 'object') {
-        // Item
-        //   .find({ _id: req.body.itemsId })
-        //   .then(datas => {
-        //     console.log(datas);
-  
-        //     totalPrice += datas.price
-        //     if (i === itemsId.length - 1) {
-              Cart.create({
-                customerId: req.body.customerId,
-                listItem: itemsId,
-                totalPrice: totalPrice
-              })
-              .then(result => {
-                console.log('berhasil');
-  
-                res.status(200).json({ message: `Success bought many items` });
-              })
-              .catch(err => {
-                res.status(500).json({ message: err });
-              })
-            // }
-          // })
-          // .catch(err => {
-          //   console.log(err);
-  
-          // })
-      } else {
-        // Item
-        //   .findById(req.body.item)
-        //   .then(item => {
-        //     return 
-            Cart.create({
-              customerId: req.body.customerId,
-              listItem: req.body.item,
-              totalPrice: item.price
-            // })
-          })
-          .then(result => {
-            res.status(200).json({ message: `Success bought an item` });
-          })
-          .catch(err => {
-            res.status(500).json({ message: err });
-          })
+      if (typeof req.body.itemsId == 'object') {
+        console.log('banyak item');
+      // let totalPrice = 0;
+    // if (typeof req.body == 'object') {
+    //   Item
+    //     .find({ _id: req.body.itemsId })
+    //     .then(datas => {
+    //       console.log(datas);
+
+          // totalPrice += datas.price
+          // if (i === itemsId.length - 1) {
+          //   Cart.create({
+          //     customerId: req.body.customerId,
+          //     listItem: itemsId,
+          //     totalPrice: totalPrice
+          //   })
+          //   .then(result => {
+          //     console.log('berhasil');
+
+          //     res.status(200).json({ message: `Success bought many items` });
+          //   })
+          //   .catch(err => {
+          //     res.status(500).json({ message: err });
+          //   })
+          // }
+        // })
+    //     .catch(err => {
+    //       console.log(err);
+
+    //     })
+      // } else {
+      //   Cart.create({
+      //     customerId: decode.id,
+      //     listItem: req.body.itemsId,
+      //     totalPrice: req.body.totalPrice
+      //   })
+      //   .then(result => {
+      //     res.status(200).json({ message: `Success bought an item` });
+      //   })
+      //   .catch(err => {
+      //     res.status(500).json({ message: err });
+      //   })
       }
     })
 
